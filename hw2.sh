b5c45c7509298935f86c35e1520f101caa323d15
xingyaxing <56070563+xingyaxing@users.noreply.github.com>
2020-04-02 12:27:14 +0800

    Delete hw2.sh

--- hw2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Configuration env
-sudo apt-get install python3-pip
-pip install --user future
-sudo apt-get install python-tk
-# install mavlink
-git clone https://github.com/mavlink/mavlink.git
-git submodule update --init --recursive
-PYTHONPATH=your_path_to_mavlink_clone
